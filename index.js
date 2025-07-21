9d6fba51fae5723f2581bb0fe68cf84120629c87
sabbas-ctrl <sabbbas.a30@gmail.com>
2025-07-21 06:02:59 +0500

    Create new user Feature Done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const { ObjectId } = require('bson');
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 app.use(cors())
@@ -8,8 +10,16 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+app.use(bodyParser.urlencoded({extended: false}))
 
-
+app.post("/api/users/", (req, res) => {
+  const newId = new ObjectId();
+  const b = req.body
+  res.json({
+    username: b.username,
+    _id: newId.toHexString()
+  })
+})
 
 
 
