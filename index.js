4e1bea55370df72ac0eabfa118a05642fba6e1b0
sabbas-ctrl <sabbbas.a30@gmail.com>
2025-07-21 20:59:34 +0500

    Add exercises feature done.

--- index.js
+++ index.js
@@ -12,16 +12,62 @@ app.get('/', (req, res) => {
 });
 app.use(bodyParser.urlencoded({extended: false}))
 
+const arrayOfUsers = []
+
 app.post("/api/users/", (req, res) => {
-  const newId = new ObjectId();
   const b = req.body
+  const user = {
+  username: "",
+  _id: new ObjectId().toHexString(),
+  logs: []
+}
+  user.username = b.username
+  arrayOfUsers.push(user)
   res.json({
-    username: b.username,
-    _id: newId.toHexString()
+    username: user.username,
+    _id: user._id
+})
+})
+app.get("/api/users", (req, res) => {
+  res.send(arrayOfUsers.map(user => {
+    return {
+        username: user.username,
+        _id: user._id
+    };
+}))
+})
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const id = req.params._id
+  const d = req.body
+  const u = arrayOfUsers.find(user => user._id === id)
+  u.logs.push({
+    description: d.description,
+    duration: parseInt(d.duration),
+    date: d.date
+  })
+
+console.log(arrayOfUsers)
+
+  res.json({
+    username: u.username,
+    _id: u._id,
+    description: d.description,
+    date: d.date,
+    duration: parseInt(d.duration)
   })
 })
 
-
+app.get("/api/users/:_id/logs?", (req, res) => {
+  const id = req.params._id
+  const u = arrayOfUsers.find(user => user._id === id)
+  res.json({
+    username: u.username,
+    _id: u._id,
+    count: arrayOfUsers.length,
+    logs: u.logs
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
